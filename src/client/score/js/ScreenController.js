d07b698af0123d958ab977aace3a07c1c50663b4
root <root@AD-WEBSERVER02.nclan.netcompany.dk>
2014-09-25 19:52:56 +0200

    asdfasdfas

---
+++ src/client/score/js/ScreenController.js
@@ -0,0 +1,13 @@
+function ScreenController($rootScope, $scope, $$screenApplication, $$websocketService){
+	$scope.$on("COMMUNICATION_INITIALIZED", function(event, data){
+		// the communication is up, let's send something
+		$$websocketService.send("Hej");
+		//$$websocketService.join();
+	});
+
+	$rootScope.broadcastEvent = function(eventname, args){
+		//logDebug("Broadcasting controller event: '{0}'".format(eventname));
+		$rootScope.$broadcast(eventname, args);
+		$rootScope.$digest();
+	};
+}

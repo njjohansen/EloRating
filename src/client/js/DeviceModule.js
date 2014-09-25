4fd0346a19ed0cdae60492b0980df4e754e71bf0
root <root@AD-WEBSERVER02.nclan.netcompany.dk>
2014-09-25 19:23:38 +0200

    deleted

--- src/client/js/DeviceModule.js
+++
@@ -1,5 +0,0 @@
-angular.module('deviceModule',['ngRoute','IoC']).config( function($routeProvider){
-	$routeProvider.
-		when('/', {controller:DeviceController}).
-		otherwise({redirectTo:'/'});
-});
--- src/client/js/IoCModule.js
+++
@@ -1,5 +0,0 @@
-angular.module('IoC',[])
-	.service('$$screenApplication', ScreenApplication)
-	.service('$$deviceApplication', DeviceApplication)
-	.service('$$websocketService', WebsocketService)
-	;

51c82fc85e153be0caf74318ead6e9f5fa1f374d
Jonas Johansen <jonasjohansen@gmail.com>
2014-09-25 22:16:11 +0200

    mere data

--- src/server/WebsocketServer.js
+++ src/server/WebsocketServer.js
@@ -1,20 +1,47 @@
 var socketIO = require('socket.io')
 var config = require('./config')
+var RatingRepository = require('./RatingRepository');
 
 var WebsocketServer = function(httpServer){
 	var self = this;
 	self.webSocket = null;
+	var _ratingRepo = new RatingRepository("set1");
 
-	var broadcastBoardUpdate = function() {
+	var broadcastUpdate = function(state) {
 		ratingState = {
 			teams: [
-				makeTeam("Team 1337"),
-				makeTeam("Team tosser"),
-				makeTeam("The tokens of fortune"),
-				makeTeam("Team Albani"),
-				makeTeam("Team FTW!"),
-				makeTeam("Team arabiske nisser"),
-				makeTeam("Team pilsner")
+				createTeam("Team 1337"),
+				createTeam("Team tosser"),
+				createTeam("The tokens of fortune"),
+				createTeam("Team Albani"),
+				createTeam("Team FTW!"),
+				createTeam("Team arabiske nisser"),
+				createTeam("Team pilsner"),
+				createTeam("Team pilsner"),
+				createTeam("Team 1337"),
+				createTeam("Team tosser"),
+				createTeam("The tokens of fortune"),
+				createTeam("Team Albani"),
+				createTeam("Team FTW!"),
+				createTeam("Team arabiske nisser"),
+				createTeam("Team pilsner"),
+				createTeam("Team pilsner"),
+				createTeam("Team 1337"),
+				createTeam("Team tosser"),
+				createTeam("The tokens of fortune"),
+				createTeam("Team Albani"),
+				createTeam("Team FTW!"),
+				createTeam("Team arabiske nisser"),
+				createTeam("Team pilsner"),
+				createTeam("Team pilsner"),
+				createTeam("Team 1337"),
+				createTeam("Team tosser"),
+				createTeam("The tokens of fortune"),
+				createTeam("Team Albani"),
+				createTeam("Team FTW!"),
+				createTeam("Team arabiske nisser"),
+				createTeam("Team pilsner"),
+				createTeam("Team pilsner")
 			]
 		};
 
@@ -22,7 +49,7 @@ var WebsocketServer = function(httpServer){
 	};
 
 	setInterval(function(){
-		broadcastBoardUpdate();
+		broadcastUpdate();
 	},4000);
 	
 	// contains all the connected client sockets
@@ -43,22 +70,42 @@ var WebsocketServer = function(httpServer){
 			//@webSocketEvent.send('New WebSocket Connection (to event subscribers)')
 			//clientSocket.send('Hi new client!')
 			clientSocket.on("EVENT", function(inputObj) {
-				clientSocket.emit("EVENT", inputObj)	
+				clientSocket.emit("EVENT", inputObj);	
 			});
 
 			clientSocket.on("MATCHUP", function(inputObj) {
-				broadcastBoardUpdate();				
+				broadcastUpdate();				
 			});						
 			
 			clientSocket.on('disconnect', function(){
 				logDebug('WS connection ended');
 			});
 
-			broadcastBoardUpdate();
+			broadcastUpdate();
 		});
 	}
 
-	var makeTeam = function(name) {
+	var registerMatchUp = function(team1, team2, winner){
+		try{
+			// load ratings
+			_ratingRepo.readRatings(function(ratings){
+				if( typeof ratings == "undefined" || ratings == null)
+					ratings = []; // initialize ratings
+
+
+			});
+			// create teams if new
+			// apply points
+			// save ratings
+			// broadcast ratings
+		}
+		catch(ex){
+			//suppress
+		}
+		broadcastUpdate();
+	};
+
+	var createTeam = function(name) {
 		return {
 			name: name,
 			rating: 1000,
@@ -71,3 +118,5 @@ var WebsocketServer = function(httpServer){
 }
 
 module.exports = WebsocketServer;
+
+{team1: "team1name", team2: "team2name", winner: }
